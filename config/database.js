8de85879dd516371e280119f4788744fc0b5ca19
Nilupulee <nilupulee.hansamali@gmail.com>
2019-02-01 01:36:32 +0530

    First commit

---
+++ config/database.js
@@ -0,0 +1,7 @@
+const conDetails = {
+    host:'sql12.freemysqlhosting.net',
+    user:'sql12276123',
+    password:'Rkgu11UnRx',
+    database:'sql12276123'
+} // store mysql database connection details for user database connection use
+module.exports.conDetails = conDetails;
